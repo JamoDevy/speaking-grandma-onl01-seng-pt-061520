959f0b72a939bb9a6dcfcf5529368ac432740ab7
Devin Clarke <devinlclarke@gmail.com>
2020-06-18 21:55:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,15 +1,4 @@
-speak_to_grandma = "I love you Grandma!".upcase
 
-  if speak_to_grandma == "I love you Grandma!".downcase
-    puts "HUH?! SPEAK UP, SONNY!"
-  
-  elsif speak_to_grandma == "I love you Grandma!".upcase
-    puts "I LOVE YOU TOO PUMPKIN!"
-    
-  else speak_to_grandma == "I love you grandma!"
-    puts "NO, NOT SINCE 1938!"
-    
-end
 
 
 
